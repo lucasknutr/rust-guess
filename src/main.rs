fdcc861e31d29203a6cfb861596dcd03fff52db2
Lucas <lucasclaudinocc@gmail.com>
2024-01-27 12:35:15 -0300

    added simple variables and imported std::io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut name = String::new();
+
+    println!("What's your name?");
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Oh no, you don't have a name. ERROR!");
+
+    println!("Guess a number between 1 and 10");
+
+    println!("Please input your guess.");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("You guessed: {input}. Thank you, {name}");
 }
